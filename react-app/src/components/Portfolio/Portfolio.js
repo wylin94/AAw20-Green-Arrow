107087cc88f23adc9757d07bbaf1bb317acf2e8e
Wei-Yang Lin <wylin94@gmail.com>
2021-11-18 15:13:24 -0800

    buy function plus porfolio display

--- react-app/src/components/Portfolio/Portfolio.js
+++ react-app/src/components/Portfolio/Portfolio.js
@@ -11,9 +11,43 @@ import './Portfolio.css'
 function Portfolio() {
 	const dispatch = useDispatch();
 	const user = useSelector(state => state.session.user);
-	const portfolios = useSelector(state => state.portfolio.portfolios?.filter(ele => ele.user_id === user.id));
-	const watchlists = useSelector(state => state.watchlist.watchlists);
+
 	const stocks = useSelector(state => Object.values(state.stock));
+	const findStockPrice = ticker => {
+		const stock = stocks?.find(stock => stock.symbol === ticker)
+		return (stock?.askPrice !== 0) ? stock?.askPrice:stock?.lastSalePrice;
+	}
+
+	const portfolios = useSelector(state => state.portfolio.portfolios?.filter(ele => ele.user_id === user.id));
+	// const portfoliosRunningBalance = portfolios?.reduce((a, b) => a + (b.purchase_price * b.share), 0)
+	const portfoliosRunningBalance = portfolios?.reduce((a, b) => a + (findStockPrice(b.ticker) * b.share), 0)
+	
+
+	const combinedPortfolios = () => {
+		let result = []
+		for (let i = 0; i < portfolios?.length; i++) {
+			if (result.length === 0) {
+				result.push({...portfolios[i]})
+				continue;
+			}
+			if (result.find(ele => ele.ticker === portfolios[i].ticker)) {
+				result.forEach(ele => {
+					if (ele.ticker === portfolios[i].ticker) {
+						ele.share = ele.share + portfolios[i].share
+					}
+				})
+			} else {
+				result.push({...portfolios[i]})
+			}
+		}
+		return result;
+	}
+	
+
+
+
+	const watchlists = useSelector(state => state.watchlist.watchlists);
+	
 
 	useEffect(() => {
 		dispatch(getPortfolios());
@@ -30,7 +64,7 @@ function Portfolio() {
 				<div className='pfStockFeed'>
 					<div className='pfgraphSection'>
 						<div className='pfgraphBalance'>
-							<div>$100,000</div>
+							<div>${(user.buying_power + portfoliosRunningBalance).toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")  }</div>
 							<div>today's change</div>
 						</div>
 						<div className='pfstockGraph'>
@@ -49,17 +83,18 @@ function Portfolio() {
 					<div className='pfStockList'>
 						<div className='pfStockListHeader'>Stocks</div>
 						<div className='pfStockListBody'>
-							{portfolios?.map(portfolio => {
+							{combinedPortfolios()?.map(portfolio => {
 								return (
 									<div className='pfStockListItemContainer' key={portfolio.id}>
 										<NavLink to={`/stocks/${portfolio.ticker}`}>
 											<div className='pfStockListItem'>
 												<div>{portfolio.ticker}</div>
+												<div>{portfolio.share} shares</div>
 												<div>chart</div>
 												<div>{
-													(stocks.find(stock => stock.symbol === portfolio.ticker)?.askPrice !== 0) ?
+													((stocks.find(stock => stock.symbol === portfolio.ticker)?.askPrice !== 0) ?
 														stocks.find(stock => stock.symbol === portfolio.ticker)?.askPrice : 
-														stocks.find(stock => stock.symbol === portfolio.ticker)?.lastSalePrice
+														stocks.find(stock => stock.symbol === portfolio.ticker)?.lastSalePrice)?.toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
 												}</div>
 											</div>
 										</NavLink>
